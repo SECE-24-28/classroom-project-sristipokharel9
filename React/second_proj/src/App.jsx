5b784549e64dd0bd7e4c80d6cb3a1a370009cc12
Sristi <sristipokharel.2024cse@sece.ac.in>
2025-12-09 09:07:09 +0530

    Fix React components - Add Content/Header exports and fix duplicate num variable

--- React/second_proj/src/App.jsx
+++ React/second_proj/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-
+import Content from './Content'
+import Header from './Header'
 function App() {
 
   let city = "Kathmandu";
-  let bums = [11, 22, 33];
+  let nums = [11, 22, 33];
   let age = 15;
   let frnds=["Smith","Victor","Nelson","Rohit"]
 
@@ -12,36 +13,37 @@ function App() {
 
   return (
     <>
-      <h1>I am from {city}</h1>
-      <p>Lucky numbers: {bums.join(", ")}</p>
-      <p>{ true ? "TRUE" : "FALSE"}</p>
-      <p>{JSON.stringify({ name: "Rohit", age: 40 })}</p>
-      <p>{age >= 18 ? "ADULT" : "MINOR"}</p>
+      {/* Example demo UI */}
+      {/* 
+        <h1>I am from {city}</h1>
+        <p>Lucky numbers: {bums.join(", ")}</p>
+        <p>{ true ? "TRUE" : "FALSE"}</p>
+        <p>{JSON.stringify({ name: "Rohit", age: 40 })}</p>
+        <p>{age >= 18 ? "ADULT" : "MINOR"}</p>
 
-      <h2>State value: {num}</h2>
+        <h2>State value: {num}</h2>
 
-      {/* Increase Button */}
-      <button onClick={() => setNum(num + 1)}>
-        Increase
-      </button>
+        <button onClick={() => setNum(num + 1)}>Increase</button>
+        <button onClick={() => setNum(num - 1)} style={{ marginLeft: "10px" }}>
+          Decrease
+        </button>
 
-      {/* Decrease Button */}
-      <button onClick={() => setNum(num - 1)} style={{ marginLeft: "10px" }}>
-        Decrease
-      </button>
-
-      {
-        frnds.map((frnd)=>(
-          <p>{frnd}</p>
-        ))
-      }
-      {
-        frnds.map((val,indx)=>(
+        {frnds.map((frnd)=> (<p>{frnd}</p>))}
+        
+        {frnds.map((val,indx)=> (
           <p key={indx}>{val}</p>
-        ))
-      }
+        ))}
+      */}
+
+      <Header title={"Friends List"} city={"Chennai"} />
+
+      {frnds.length > 0 ? (
+        <Content frnds={frnds} />
+      ) : (
+        <p>List is Empty</p>
+      )}
     </>
-  );
+  )
 }
 
 export default App;
---
+++ React/second_proj/src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header=({title,city})=>
+{
+    return(
+        <div>
+            <h1>{title}-{city}</h1>
+            </div>
+    )
+}
+export default Header
